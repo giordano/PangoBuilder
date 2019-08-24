77ea1dbda5d8581bff2b101e22c8788e91a401e0
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-08-24 18:24:38 +0200

    Add autogenerated files to build Pango

---
+++ README.md
@@ -0,0 +1,9 @@
+# PangoBuilder
+
+[![Build Status](https://travis-ci.org/giordano/PangoBuilder.svg?branch=master)](https://travis-ci.org/giordano/PangoBuilder)
+
+This repository builds binary artifacts for the Pango project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/giordano/PangoBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,71 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "Pango"
+version = v"1.44.5"
+
+# Collection of sources required to build Pango
+sources = [
+    "http://ftp.gnome.org/pub/GNOME/sources/pango/1.44/pango-1.44.5.tar.xz" =>
+    "8527dfcbeedb4390149b6f94620c0fa64e26046ab85042c2a7556438847d7fc1",
+    "https://github.com/mesonbuild/meson/releases/download/0.51.1/meson-0.51.1.tar.gz" =>
+    "f27b7a60f339ba66fe4b8f81f0d1072e090a08eabbd6aa287683b2c2b9dd2d82",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/pango-*/
+MESON="$(readlink -f ../meson-0.*/meson.py)"
+sed -i 's/true/false/g' meson_options.txt
+mkdir build
+cd build
+
+cat << EOF > cross_compile.txt
+[binaries]
+c = '${CC}'
+cpp = '${CXX}'
+ar = '${AR}'
+strip = '${STRIP}'
+pkgconfig = '/usr/bin/pkg-config'
+
+[host_machine]
+system = 'linux'
+cpu_family = 'x86_64'
+cpu = 'i686'
+endian = 'little'
+
+[target_machine]
+system = 'linux'
+cpu_family = 'x86'
+cpu = 'i686'
+endian = 'little'
+
+[paths]
+prefix = '${prefix}'
+libdir = 'lib'
+bindir = 'bin'
+EOF
+
+$MESON .. --cross-file cross_compile.txt
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:x86_64, libc=:glibc)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libpango", :libpango)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
